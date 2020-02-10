3ab41e7c19b86d10024d513ab9cbafa85fee20c8
MuhamadRyanHidayat <ryanhidayat074@gmail.com>
2020-02-10 12:48:54 +0700

    Aduan

---
+++ app/Http/Controllers/AspirasiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Aspirasi;
+use Illuminate\Http\Request;
+
+class AspirasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('pages.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Aspirasi  $aspirasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aspirasi $aspirasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Aspirasi  $aspirasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aspirasi $aspirasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Aspirasi  $aspirasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Aspirasi $aspirasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Aspirasi  $aspirasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aspirasi $aspirasi)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="frontend/libraries/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend/styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300i,400,400i,500,700,800&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+  
+        
+                <div class="navbar">
+                    <div class="container">
+                    <div class="col-md-4 col-sm-12 col-12">
+                       <a href="" class="navbar-brand text-white">LOGO</a>
+                    </div>
+                    <div class="col-md-4 col-12 text-center">
+                        <h2 class="my-md-3 site-title text-white">Aduan Siswa</h2>
+                    </div>
+                    <div class="col-md-4 col-12 text-right">
+                        <p class="my-md-4 header-links">
+                            <a href="#" class="px-2 text-white">Login</a>
+                            <a href="#" class="px-1 text-white">Register</a>
+                        </p>
+                    </div>
+                    </div>
+                </div>
+            
+
+        <div class="hero">
+        <div class="banner">
+            <div class="left-column">
+                <h1>Aduan<span>Siswa</span></h1>
+                <h3>Layanan Aspirasi<span> SMK Jakarta Pusat 1</span>
+                </h3>
+                <p>
+                    AduanSiswa adalah Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam doloribus eligendi molestiaseius, animi voluptatibus repellat 
+                </p>
+                
+                <div class="btn">
+                    <a href="" class="button text-decoration-none">Tentang Kami</a>
+                    <a href="" class="button text-decoration-none">Kirim Laporan</a>
+                    
+                </div>
+            </div>
+            <div class="right-column">
+                <img src="frontend/images/studentbg.jpg" class="img-fluid" alt="Responsive Image">
+            </div>
+        </div>
+    </div>
+
+
+    <script src="frontend/libraries/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
